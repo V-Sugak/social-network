1dfc630e6ace28ae2b98b381f549e940f08b71cd
V-Sugak <vicka.sugak2010@yandex.ru>
2022-01-14 20:01:42 +0300

    login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from './Header.module.css';
 
-export const Header = () => {
+type HeaderPropsType = {
+    isAuth: boolean
+    login: string | null
+}
+
+export const Header = (props: HeaderPropsType) => {
     return (
         <header className={s.head}>
             <div className={s.logo}>
                 By Viktoria...
                 <span className={s.loginBlock}>
-                <NavLink to={'/login'}>LOGIN</NavLink>
-            </span>
+                    {props.isAuth ? props.login : <NavLink to={'/login'}>LOGIN</NavLink>}
+                           </span>
             </div>
         </header>
     )
